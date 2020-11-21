bca1af78ae0ffa24581ec63c27c1ddf2f5c36639
Mr.Z_Ms.L <942489192@qq.con>
2020-11-21 16:50:50 +0800

    基础摸板

---
+++ day_2/sirfang/vue.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    devServer : {
+        //端口号
+        port: 8080,
+        //主机名称
+        host: "localhost",
+        //http协议
+        https: false,
+        //是否自动打开浏览器
+        open: true,
+        //配置跨域
+        // proxy: {
+        //     "dev-api": {
+        //         // 跨域地址
+        //         target: "http://localhost:8080",
+        //         // 是否开启代理
+        //         changeOrgin: true,
+        //         // 重写地址
+        //         pathRewrite: {
+        //             "^dev-api": ""
+        //         }
+        //     }
+        // }
+    }
+}
